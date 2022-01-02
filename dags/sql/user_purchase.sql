e6cca588400040cf0444239f43362c2cef2424c5
paulirat4 <ana.rendon@wizeline.com>
2022-01-02 01:24:49 -0600

    just left create schema

--- dags/sql/user_purchase.sql
+++ dags/sql/user_purchase.sql
@@ -1,11 +1,1 @@
 CREATE SCHEMA deb_paulirat;
-CREATE TABLE deb_paulirat.user_purchase(
-invoice_number varchar(10),
-stock_code varchar(20),
-detail varchar(1000),
-quantity int,
-invoice_date timestamp,
-unit_price numeric(8,3),
-customer_id int,
-country varchar(20)
-);

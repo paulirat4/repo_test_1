cc2d6013d6b604fd48126975c9fbb53de33caa13
paulirat4 <ana.rendon@wizeline.com>
2022-01-16 18:31:06 -0600

    c

--- hello_world_airflow.py
+++ hello_world_airflow.py
@@ -12,16 +12,16 @@ def print_welcome():
     return "Welcome from custom operator - Airflow DAG!"
 
 
-welcome_operator = PythonOperator(
-    task_id="welcome_task", python_callable=print_welcome, dag=dag
-)
-
 dag = DAG(
     "hello_world",
     description="Hello World DAG",
     schedule_interval="0 12 * * *",
     start_date=datetime(2017, 3, 20),
     catchup=False,
+)
+
+welcome_operator = PythonOperator(
+    task_id="welcome_task", python_callable=print_welcome, dag=dag
 )
 
 hello_operator = PythonOperator(

c821f9e60fc02200ebc69ab91610e0f50a593a6d
paulirat4 <ana.rendon@wizeline.com>
2022-01-16 23:31:03 -0600

    miau

--- dbname.products.sql
+++ dbname.products.sql
@@ -6,20 +6,12 @@ DROP TABLE IF EXISTS dbname.products;
 
 CREATE TABLE IF NOT EXISTS dbname.products
 (
-    id SERIAL PRIMARY KEY,
-    producto VARCHAR(255),
-    presentacion VARCHAR(255),
-    marca VARCHAR(255),
-    categoria VARCHAR(255),
-    catalogo VARCHAR(255),
-    precio FLOAT,
-    fecharegistro DATE,
-    cadenacomercial VARCHAR(255),
-    giro VARCHAR(255),
-    nombrecomercial VARCHAR(255),
-    direccion VARCHAR(255),
-    estado VARCHAR(255),
-    municipio VARCHAR(255),
-    latitud FLOAT,
-    longitud FLOAT    
+    invoice_number VARCHAR(10),
+    stock_code VARCHAR(20),
+    detail VARCHAR(1000),
+    quantity INT,
+    invoice_date TIMESTAMP,
+    unit_price NUMERIC(8,3),
+    customer_id INT,
+    country VARCHAR(20) 
 );

bf581faf015c94bc2dc31438a4a773e86047df54
paulirat4 <ana.rendon@wizeline.com>
2022-01-17 21:54:52 -0600

    JGHG

--- dbname.products.sql
+++ dbname.products.sql
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS dbname.products
     invoice_number VARCHAR(10),
     stock_code VARCHAR(20),
     detail VARCHAR(1000),
-    quantity INT,
+    quantity BIGINT,
     invoice_date TIMESTAMP,
     unit_price NUMERIC(10,5),
-    customer_id INT,
+    customer_id BIGINT,
     country VARCHAR(20) 
 );

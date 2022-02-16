2ec647f4da0563d64b53fa073f2227bc9b382e85
paulirat4 <ana.rendon@wizeline.com>
2022-02-16 00:04:41 -0600

    change

--- dags/load_products_dag.py
+++ dags/load_products_dag.py
@@ -541,7 +541,7 @@ class fromS3toS3TabDelimited(BaseOperator):
         # user_purchase_str = re.sub(" 536" , '536', user_purchase_str)
         user_purchase_str = 'miau'
 
-        self.s3.load_string(bucket_name=self.s3_bucket, key="user_purchase_tab_del.txt", string_data=user_purchase_str)
+        #self.s3.load_string(bucket_name=self.s3_bucket, key="user_purchase_tab_del.txt", string_data=user_purchase_str)
 
 
 

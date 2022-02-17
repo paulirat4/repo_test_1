0d5fe0bf45a0d0a39eefacb582b9d24482a6e959
paulirat4 <ana.rendon@wizeline.com>
2022-02-17 12:37:05 -0600

    added correct path

--- dags/load_products_dag.py
+++ dags/load_products_dag.py
@@ -143,8 +143,8 @@ class S3ToPostgresTransfer(BaseOperator):
         print(os.path.sep)
 
         #ruta_archivo = +os.path.sep + nombre_de_archivo
-        ruta_archivo = str(os.path.sep) + nombre_de_archivo
-        #ruta_archivo = "/opt/airflow/dbname.products.sql"
+        #ruta_archivo = str(os.path.sep) + nombre_de_archivo
+        ruta_archivo = "/opt/airflow/dbname.user_purchase.sql
         #ruta_archivo = "/Users/ana.rendon/airflow/dags/dbname.products.sql"
 
 

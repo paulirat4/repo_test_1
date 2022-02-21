4f9d20d2522d437eb120e542ccf4f642e27e85cc
paulirat4 <ana.rendon@wizeline.com>
2022-02-20 20:53:43 -0600

    changed task order

--- dags/load_products_dag.py
+++ dags/load_products_dag.py
@@ -699,7 +699,7 @@ from_s3_to_s3_tab_delimited= fromS3toS3TabDelimited(
 #from_s3_to_s3
 #trigger_glue_job_log_reviews
 #postgres_to_s3
-from_s3_to_s3_tab_delimited >> s3_to_postgres_operator >> trigger_glue_job_log_reviews
+from_s3_to_s3_tab_delimited >> s3_to_postgres_operator >> trigger_glue_job_movies_reviews
 #from_s3_to_s3_tab_delimited .set_downstream(s3_to_postgres_operator)
 #from_s3_to_s3_tab_delimited
 # welcome_operator  # .set_downstream(s3_to_postgres_operator)

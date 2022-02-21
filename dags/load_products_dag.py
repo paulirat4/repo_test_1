80e804a4f1a0461c3feb2f4fafa8caecf5ad094e
paulirat4 <ana.rendon@wizeline.com>
2022-02-20 20:51:28 -0600

    changed trigger_glue_job_movies_reviews bucket

--- dags/load_products_dag.py
+++ dags/load_products_dag.py
@@ -654,9 +654,9 @@ trigger_glue_job_movies_reviews = AwsGlueJobOperator(
     job_name="etl-job-movies-reviews-",
     iam_role_name="glue_role_paulirat",
     task_id="job",
-    dag=dag2,
-    script_args= {'--example_movie_review_path':   's3://s3-data-bootcamp-20220202174438981400000001/example_movies_reviews.csv',
-    '--bucket_for_processed_data_path':   's3://processed-data-bucket-20220202174438981500000003/processed_data'}
+    dag=dag1,
+    script_args= {'--example_movie_review_path':   's3://s3-data-bootcamp-20220221000156271300000008/example_movies_reviews.csv',
+    '--bucket_for_processed_data_path':   's3://processed-data-bucket-20220221000156270900000006/movie_reviews'}
         )
 
 trigger_glue_job_log_reviews = AwsGlueJobOperator(
@@ -699,7 +699,7 @@ from_s3_to_s3_tab_delimited= fromS3toS3TabDelimited(
 #from_s3_to_s3
 #trigger_glue_job_log_reviews
 #postgres_to_s3
-from_s3_to_s3_tab_delimited >> s3_to_postgres_operator
+from_s3_to_s3_tab_delimited >> s3_to_postgres_operator >> trigger_glue_job_log_reviews
 #from_s3_to_s3_tab_delimited .set_downstream(s3_to_postgres_operator)
 #from_s3_to_s3_tab_delimited
 # welcome_operator  # .set_downstream(s3_to_postgres_operator)

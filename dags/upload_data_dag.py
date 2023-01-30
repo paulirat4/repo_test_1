d89a8886ba3cfb8a82be383b656af513657d1f4c
paulirat4 <ana.rendon@wizeline.com>
2023-01-30 00:02:58 -0600

    added current buckets and changed user purchase data mini for user purchase (mini anyway)

--- dags/upload_data_dag.py
+++ dags/upload_data_dag.py
@@ -292,7 +292,7 @@ with DAG(
     schema="dbname",  #'public'
     table="user_purchase",
     # s3_bucket="bucket-test-45",
-    s3_bucket="s3-data-bootcamp-20230129200934367500000007",  
+    s3_bucket="s3-data-bootcamp-20230130001153688600000009",  
     # s3_key="test_1.csv",
     s3_key="user_purchase_tab_delimited.txt",
     aws_conn_postgres_id="postgres_default",
@@ -300,12 +300,14 @@ with DAG(
     #dag=dag1
     )
 
+    
+
     from_s3_to_s3= fromS3toS3XML(
     task_id="from_s3_to_s3",
     schema="dbname",  #'public'
     table="user_purchase",
     # s3_bucket="bucket-test-45",
-    s3_bucket="s3-data-bootcamp-20230129200934367500000007",
+    s3_bucket="s3-data-bootcamp-20230130001153688600000009",
     # s3_key="test_1.csv",
     s3_key="log_reviews.csv",
     aws_conn_postgres_id="postgres_default",
@@ -318,10 +320,10 @@ with DAG(
     schema="dbname",  #'public'
     table="user_purchase",
     # s3_bucket="bucket-test-45",
-    s3_bucket="s3-data-bootcamp-20230129200934367500000007",
+    s3_bucket="s3-data-bootcamp-20230130001153688600000009",
     # s3_key="test_1.csv",
-    #s3_key="user_purchase_data.csv",
-    s3_key="user_purchase_data_mini.csv",
+    s3_key="user_purchase_data.csv",
+    #s3_key="user_purchase_data_mini.csv",
     aws_conn_postgres_id="postgres_default",
     aws_conn_id="aws_default",
     #dag=dag1
